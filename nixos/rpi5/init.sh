75025fdbda7c8d46055fab7bc6e9b48b851ac654
Mountain Star <github@mtnstar.net>
2025-05-07 21:39:38 +0200

    Try to make efi work

--- nixos/rpi5/init.sh
+++ nixos/rpi5/init.sh
@@ -2,4 +2,5 @@
 
 mkdir -p /tmp/vagrant/edk2
 cp /usr/share/edk2/aarch64/QEMU_VARS.fd /tmp/vagrant/edk2/myvm_VARS.fd
-#dd if=/dev/zero of=/tmp/vagrant/edk2/myvm_VARS.fd bs=1M count=64
+cp /usr/share/edk2/aarch64/QEMU_EFI.fd /tmp/vagrant/edk2/QEMU_EFI.fd
+dd if=/dev/zero of=/tmp/vagrant/edk2/QEMU_EFI.fd bs=1c count=1 seek=67108863
